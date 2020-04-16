2097d3ce7cf29f00fa4ad1f17c4bd323e5c37e3c
Tim Chumbash <posseslash@gmail.com>
2020-04-16 22:41:53 +0200

    module3-task1 updates

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,12 +1,18 @@
+import {getRandomArrayItem} from "../utils";
+
 const filterNames = [
   `all`, `overdue`, `today`, `favorites`, `repeating`, `archive`
+];
+
+const filterValues = [
+  `11`, `69`, `13`, `7`, `42`, `15`, `34`
 ];
 
 const generateFilters = () => {
   return filterNames.map((it) => {
     return {
       name: it,
-      count: Math.floor(Math.random() * 15),
+      count: getRandomArrayItem(filterValues)
     };
   });
 };
--- src/mock/task.js
+++ src/mock/task.js
@@ -1,4 +1,6 @@
 import {COLORS} from "../consts";
+import {getRandomArrayItem, getRandomDate} from "../utils";
+
 
 const DefaultRepeatingDays = {
   "mo": false,
@@ -16,25 +18,7 @@ const DescriptionItems = [
   `Пройти интенсив на соточку`,
 ];
 
-const getRandomIntegerNumber = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min));
-};
 
-const getRandomArrayItem = (array) => {
-  const randomIndex = getRandomIntegerNumber(0, array.length);
-
-  return array[randomIndex];
-};
-
-const getRandomDate = () => {
-  const targetDate = new Date();
-  const sign = Math.random() > 0.5 ? 1 : -1;
-  const diffValue = sign * getRandomIntegerNumber(0, 8);
-
-  targetDate.setDate(targetDate.getDate() + diffValue);
-
-  return targetDate;
-};
 
 const generateRepeatingDays = () => {
   return Object.assign({}, DefaultRepeatingDays, {
